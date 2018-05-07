fea76f7255652029dacc3e0e6ab7f4e22f352446
105824 <a.andrzejewska@poczta.fm>
2018-05-07 22:19:05 +0200

    Pierwsza wersja pliku

--- kalkulator.py
+++ kalkulator.py
@@ -11,20 +11,4 @@ for i in range(0, int(input)):
         else:
             tablica.append(tablica[i-2] + tablica[i-1])
 
-# program pesel
-pesel = input()
-
-wagi = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
-my_sum = 0
-
-for i in range(0, len(wagi)):
-    my_sum += int(pesel[i]) * wagi[i]
-
-result = 10 - (my_sum % 10)
-if int(pesel[10]) == result:
-    print(1)
-else:
-    print(0)
-
-
 print(tablica)
