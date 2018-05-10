72f7dd3be386e6bd87fe29580940c226ea25c6a0
105824 <a.andrzejewska@poczta.fm>
2018-05-10 12:31:08 +0200

    Druga wersja pliku

---
+++ kalkulator.py
@@ -0,0 +1,12 @@
+def dodawanie():
+    a = raw_input("Pierwsza liczba ")
+    b = raw_input("Druga liczba ")
+    wynik = float(a)+float(b)
+    return wynik
+
+def odejmowanie():
+    a = raw_input("Pierwsza liczba ")
+    b = raw_input("Druga liczba ")
+    wynik = float(a)-float(b)
+    return wynik
+
